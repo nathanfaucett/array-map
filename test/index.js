2156ba60ee658c7979799ef2e2622ca24feae2f7
nathanfaucett <nathanfaucett@gmail.com>
2015-08-16 10:41:34 -0500

    updates

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var tape = require("tape"),
     arrayMap = require("..");
 
 
-tape("arrayMap(object) creates a new array with the results of calling a provided function on every element in this array", function(assert) {
+tape("arrayMap(array, callback) creates a new array with the results of calling a provided function on every element in this array", function(assert) {
     assert.deepEquals(
         arrayMap([0, 1, 2, 3, 4], function(value, index) {
             return value + index;
